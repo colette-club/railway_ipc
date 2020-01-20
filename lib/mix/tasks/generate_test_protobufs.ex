e5eb91b46e064ffbc83948da71f6d973e955bfcd
David Jeusette <jeusette.david@gmail.com>
2020-01-20 15:14:50 +0100

    Properly return the protoc result in the mix task

--- lib/mix/tasks/generate_test_protobufs.ex
+++ lib/mix/tasks/generate_test_protobufs.ex
@@ -3,13 +3,19 @@ defmodule Mix.Tasks.GenerateTestProtobufs do
   Genereates the test protobufs
   """
 
+  require Logger
+
   use Mix.Task
 
   def run(_arg) do
     :os.cmd(
       'protoc --proto_path=test/support/protobuf --elixir_out=test/support/messages test/support/protobuf/*.proto'
     )
-
-    IO.puts("Protobuf messages generated successfully")
+    |> case do
+      [] ->
+        Logger.info("Protobuf messages generated successfully")
+      error ->
+        Logger.error("Protobuf generation failed with error: #{inspect error}")
+    end
   end
 end
